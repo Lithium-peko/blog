e333cbb5d6088b7c7307dacf79bcad59fca72402
Lithium_peko <lithiumreona@gmail.com>
2023-03-21 23:34:27 +0800

    日志中间件

---
+++ server/router/middleware/logger.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"blog/utils"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"time"
+)
+
+// 日志中间件
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		cost := time.Since(start)
+		utils.Logger.Info(c.Request.URL.Path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("query", c.Request.URL.RawQuery),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
+		)
+	}
+}
